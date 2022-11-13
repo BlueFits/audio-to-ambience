fe33de964e531e0f924e3ec19e877377a4e70cee
bluefits <44682470+BlueFits@users.noreply.github.com>
2022-11-13 15:14:08 -0500

    Added express server

--- server/server.ts
+++ server/server.ts
@@ -1,21 +1,36 @@
-import { createServer } from 'http'
-import { parse } from 'url'
 import next from 'next'
+import express from "express";
+import cookieParser from 'cookie-parser';
+import cors from "cors";
 
 const port = parseInt(process.env.PORT || '3000', 10)
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
+const server: express.Application = express();
+
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url!, true)
-    handle(req, res, parsedUrl)
-  }).listen(port)
 
-  console.log(
-    `> Server listening at http://localhost:${port} as ${
-      dev ? 'development' : process.env.NODE_ENV
-    }`
-  )
+  server.use(express.json());
+  server.use(cors());
+  server.use(cookieParser());
+
+  server.get("/test", (req: express.Request, res: express.Response) => {
+      res.status(200).send("working");
+  });
+
+  server.all("*", (req: express.Request, res: express.Response) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, () => {
+    // our only exception to avoiding console.log(), because we
+    // always want to know when the server is done starting up
+    console.log(
+      `> Server listening at http://localhost:${port} as ${
+        dev ? 'development' : process.env.NODE_ENV
+      }`
+    )
+  })
 })
