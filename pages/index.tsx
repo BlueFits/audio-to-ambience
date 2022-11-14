67f251bdea494d83dd25834869aa100d86c5b983
bluefits <44682470+BlueFits@users.noreply.github.com>
2022-11-13 20:18:58 -0500

    Added sound classification

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,55 @@
 import Link from 'next/link'
+import RoadMap from "../assets/RoadMap.svg";
+import Image from "next/image";
+import Webcam from "react-webcam";
+import { useState } from 'react';
 
 export default function Home() {
+
+  const [isScanning, setIsScanning] = useState(false);
+
+  function gotResult(error, result) {
+    if (error) { console.log(error); return; }
+    console.log(result);
+  }
+
+  const classify = async () => {
+    try {
+      const classifier = await ml5.soundClassifier('SpeechCommands18w', { probabilityThreshold: 0.9 });
+      classifier.classify(gotResult);
+      setIsScanning(false);
+    } catch(err) {
+      console.error(err)
+    }
+  };
+
   return (
-    <ul>
-      <li>
-        <Link href="/a" as="/a">
-          Test deployment
-        </Link>
-      </li>
-      <li>
-        <Link href="/b" as="/b">
-          b
-        </Link>
-      </li>
-    </ul>
+    <div>
+      <ul>
+        <li>
+          <Link href="/a" as="/a">
+            Test deployment
+          </Link>
+        </li>
+        <li>
+          <Link href="/b" as="/b">
+            b
+          </Link>
+        </li>
+      </ul>
+
+      <div>
+        {/* <Image id="image_roadmap" alt="roadmap" src={RoadMap}/> */}
+        {/* <Webcam id="video_id"/> */}
+        <button 
+          onClick={() => {
+            setIsScanning(true);
+            classify();
+          }} 
+          disabled={isScanning ? true : false}>
+          Scan
+        </button>
+      </div>
+    </div>
   )
 }
--- server/server.ts
+++ server/server.ts
@@ -2,24 +2,30 @@ import next from 'next'
 import express from "express";
 import cookieParser from 'cookie-parser';
 import cors from "cors";
+import morgan from "morgan";
+//Router
+import Ml5Routes from "./ml5/ml5.routes.config";
+
 
 const port = parseInt(process.env.PORT || '3000', 10)
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 const server: express.Application = express();
-
+const ml5Router = new Ml5Routes("Ml5Routes").getRouter;
 
 app.prepare().then(() => {
 
+  if (dev) {
+    server.use(morgan("dev"));
+  }
   server.use(express.json());
   server.use(cors());
   server.use(cookieParser());
 
-  server.get("/test", (req: express.Request, res: express.Response) => {
-      res.status(200).send("working");
-  });
+  server.use("/ml5", ml5Router);
 
+  //Connects NextJS application
   server.all("*", (req: express.Request, res: express.Response) => {
     return handle(req, res);
   });
